ebca01bc4ade5c0ea6224a4cc929c93f90973de6
Jorge Nunes <jcvn@jcvn.com.br>
2026-01-15 18:13:59 -0300

    Fix fonts loading in production using Google Fonts

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,5 @@
 import { createRoot } from "react-dom/client";
 
-import "@fontsource/manrope/400.css";
-import "@fontsource/manrope/500.css";
-import "@fontsource/manrope/600.css";
-import "@fontsource/manrope/700.css";
-import "@fontsource/manrope/800.css";
-import "@fontsource/source-sans-3/400.css";
-import "@fontsource/source-sans-3/500.css";
-import "@fontsource/source-sans-3/600.css";
-import "@fontsource/source-sans-3/700.css";
-import "@fontsource/source-sans-3/800.css";
-
 import App from "./App.tsx";
 import "./index.css";
 
